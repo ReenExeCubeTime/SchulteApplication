fc4b6bda6c9917797be13a9bf0c03d724a0731d8
ReenExe <ipourri@gmail.com>
2016-08-19 20:19:51 +0300

    change view from code

--- app/src/main/java/ua/com/senseye/schultetableapplication/MainActivity.java
+++ app/src/main/java/ua/com/senseye/schultetableapplication/MainActivity.java
@@ -2,6 +2,7 @@ package ua.com.senseye.schultetableapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button button = (Button) findViewById(R.id.button1);
+
+        button.setText("A");
     }
 }
