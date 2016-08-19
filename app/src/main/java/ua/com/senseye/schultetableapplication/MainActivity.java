7187baa35af418669df7061c3078863401331427
ReenExe <ipourri@gmail.com>
2016-08-19 20:54:42 +0300

    first `onClick`

--- app/src/main/java/ua/com/senseye/schultetableapplication/MainActivity.java
+++ app/src/main/java/ua/com/senseye/schultetableapplication/MainActivity.java
@@ -2,17 +2,25 @@ package ua.com.senseye.schultetableapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button button = (Button) findViewById(R.id.button1);
+        button = (Button) findViewById(R.id.button1);
 
-        button.setText("A");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("A");
+            }
+        });
     }
 }
